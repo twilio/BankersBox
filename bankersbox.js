0aa107dada720792d18e9b2150ff7d76f4c603d4
Chad Etzel <chetzel@twilio.com>
2012-02-28 17:57:16 -0800

    update license stuff

--- bankersbox.js
+++ bankersbox.js
@@ -1,7 +1,7 @@
 /**
  * @license MIT License
  *
- * Copyright (c) 2012 Twilio, Chad Etzel
+ * Copyright (c) 2012 Twilio Inc., Chad Etzel
  *
  * Permission is hereby granted, free of charge, to any person obtaining
  * a copy of this software and associated documentation files (the
